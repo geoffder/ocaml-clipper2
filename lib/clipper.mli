e7b675e104688436879e6d983d7bb3c2e935c622
Geoff deRosenroll <geoffderosenroll@gmail.com>
2023-02-13 13:31:54 -0800

    Add doc workflow and index example

--- lib/clipper.mli
+++ lib/clipper.mli
@@ -1,3 +1,4 @@
-(** Bindings to the Clipper2 polygon clipping library *)
+(** Bindings to the {{:http://www.angusj.com/clipper2/Docs/Overview.htm}
+    Clipper2} polygon clipping library *)
 
 include Clipper_intf.Intf
